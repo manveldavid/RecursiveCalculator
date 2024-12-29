402a29caefed87f1d8fc04e7a0a93b468413526d
Давид <manvelyancda@masterscada.ru>
2024-12-29 15:30:01 +0300

    refactoring

--- RecursiveCalc.TelegramBot/TelegramBot/Program.cs
+++ RecursiveCalc.TelegramBot/TelegramBot/Program.cs
@@ -3,45 +3,22 @@ using RecursiveCalcEngine;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
 
-namespace TelegramBot;
+namespace TelegramBotCalc;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        var offset = 0;
+        var tgBotPollPeriodInSeconds = TimeSpan.FromSeconds(double.TryParse(Environment.GetEnvironmentVariable("TG_BOT_POLL_PERIOD_SECONDS"), out var _tgBotPollPeriodInSeconds) ? _tgBotPollPeriodInSeconds : 10d);
         var apiKey = Environment.GetEnvironmentVariable("API_KEY")!;
-        var telegramBot = new TelegramBotClient(apiKey);
-        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
         Console.WriteLine("bot run!");
 
-        while (true)
-        {
-            var updates = await telegramBot.GetUpdatesAsync(offset);
-            await Task.Delay(TimeSpan.FromSeconds(5));
-
-            foreach (var update in updates)
-            {
-                if (update is null || update.Message is null || string.IsNullOrWhiteSpace(update.Message.Text))
-                    continue;
-
-                var response = string.Empty;
-                try
-                {
-                    var result = Calc.Solve(Calc.Clean(update.Message.Text));
-                    response = $"result: `{Math.Round(result.Result, 4)}`\n\nhistory:\n{string.Join("\n",
-                        result.History.Select((l, i) => $"\t{++i}. {l.Substring(0, l.IndexOf(new CalcConfig().EqualsDigits.First()) + 1) + "`" +
-                            l.Substring(l.IndexOf(new CalcConfig().EqualsDigits.First()) + 1) + "`"}"))}".Replace("*", "\\*").Replace("+-", "-");
-                }
-                catch (Exception ex)
-                {
-                    response = ex.ToString();
-                }
-
-                await telegramBot.SendTextMessageAsync(update.Message.Chat, response, parseMode: ParseMode.Markdown);
-
-                offset = update.Id + 1;
-            }
-        }
+        await Task.WhenAll([
+                new TelegramBot().RunAsync(
+                    apiKey,
+                    tgBotPollPeriodInSeconds,
+                    CancellationToken.None)
+            ]);
     }
 }
---
+++ RecursiveCalc.TelegramBot/TelegramBot/TelegramBot.cs
@@ -0,0 +1,63 @@
+﻿using Telegram.Bot.Types;
+using Telegram.Bot;
+using RecursiveCalcEngine;
+using Telegram.Bot.Types.Enums;
+
+namespace TelegramBotCalc;
+
+public class TelegramBot
+{
+    public async Task RunAsync(string apiKey, TimeSpan pollPeriod, CancellationToken cancellationToken)
+    {
+        if (string.IsNullOrEmpty(apiKey))
+            return;
+
+        var offset = 0;
+        var telegramBot = new TelegramBotClient(apiKey);
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            await Task.Delay(pollPeriod, cancellationToken);
+
+            Update[] updates = Array.Empty<Update>();
+            try
+            {
+                updates = await telegramBot.GetUpdates(offset, timeout: (int)pollPeriod.TotalSeconds, cancellationToken: cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            if (!cancellationToken.IsCancellationRequested)
+            {
+                foreach (var update in updates)
+                {
+                    offset = update.Id + 1;
+
+                    if (update is null || 
+                        update.Message is null || 
+                        string.IsNullOrEmpty(update.Message.Text))
+                        continue;
+
+                    var response = string.Empty;
+                    try
+                    {
+                        var result = Calc.Solve(Calc.Clean(update.Message.Text));
+                        response = $"result: `{Math.Round(result.Result, 4)}`\n\nhistory:\n{string.Join("\n",
+                            result.History.Select((l, i) => $"\t{++i}. {l.Substring(0, l.IndexOf(new CalcConfig().EqualsDigits.First()) + 1) + "`" +
+                                l.Substring(l.IndexOf(new CalcConfig().EqualsDigits.First()) + 1) + "`"}"))}".Replace("*", "\\*").Replace("+-", "-");
+                    }
+                    catch (Exception ex)
+                    {
+                        response = ex.ToString();
+                    }
+
+                    await telegramBot.SendMessage(update.Message.Chat, response, parseMode: ParseMode.Markdown, replyParameters: new ReplyParameters { MessageId = update.Message.Id });
+
+                }
+            }
+        }
+    }
+}
+
